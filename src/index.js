6029997365257fa4673b17ccabae69564ccabe97
Glauber Victor Leite Carvalho <glauber.victor@tron.com.br>
2018-05-25 10:44:26 -0300

    Alterado para aceitar o ip 192.x.x.x

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ class HttpsRedirect extends React.Component {
   isLocalHost(hostname) {
     return !!(hostname === 'localhost' ||
               hostname === '[::1]' ||
-              hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/));
+              hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/) ||
+              hostname.match(/^192(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/));
   }
 
   render() {
